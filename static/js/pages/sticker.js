ffe3533e3ea188c11a00561b290b6260dc05df1f
coderadi-in <adiprofitcoder@gmail.com>
2026-08-20 12:30:45 +0530

    Updated tools window in sticker generator.

--- static/js/pages/sticker.js
+++ static/js/pages/sticker.js
@@ -4,10 +4,14 @@
 
 const barcodeInput = document.getElementById('barcodeInput');
 const generateBarcode = document.getElementById('generateBarcode');
+const barcodePreview = document.getElementById('barcodePreview');
 
+const sheetBody = document.querySelector('.sheet-window .body');
 const genIllustration = document.querySelector('.generation-illustration');
+const windowToggleTriggers = document.querySelectorAll('.unit .head');
 
-const barcodeImage = document.getElementById('barcodeImage');
+const columnInput = document.getElementById('columnInput');
+const rowInput = document.getElementById('rowInput');
 
 // ==================================================
 // STATES
@@ -59,10 +63,58 @@ function animateGenSkeleton(frame = genIllustration) {
     }
 }
 
+// * FUNCTION TO TOGGLE WINDOW STATE
+function toggleWindow(window) {
+    window.classList.toggle('active');
+}
+
+// * FUNCTION TO UPDATE BODY GRIDS
+function updateBodyGrids(x, y) {
+    sheetBody.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+    sheetBody.style.gridTemplateRows = `repeat(${y}, 1fr)`;
+}
+
+// * FUNCTION TO CREATE A STICKER ELEMENT
+function createSticker() {
+    // Create Wrapper
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('sticker', 'column', 'gap-12');
+
+    // Create Image
+    const barcodeImage = document.createElement('img');
+    barcodeImage.classList.add('img');
+    barcodeImage.src = barcodePreview.src;
+
+    // Append children
+    wrapper.appendChild(barcodeImage);
+    return wrapper
+}
+
+// * FUNCTION TO ADD INPUT-CHANGE LISTENER
+function addInputChangeListener(input) {
+    input.addEventListener('input', () => {
+        updateBodyGrids(
+            columnInput.value,
+            rowInput.value
+        );
+
+        document.querySelectorAll('.sheet-window .body .sticker').forEach(elem => {
+            elem.remove();
+        });
+
+        const stickerQuantity = rowInput.value * columnInput.value
+        for (let i = 0; i < stickerQuantity; i++) {
+            const sticker = createSticker();
+            sheetBody.appendChild(sticker);
+        }
+    });
+}
+
 // ==================================================
 // EVENT LISTENERS
 // ==================================================
 
+// & EVENT LISTENER FOR GENERATE-BARCODE CLICK
 generateBarcode.addEventListener('click', async () => {
     if (barcodeInput.value.trim() === "") {
         sendToastNotification("There's no barcode content", "barcode", "var(--color-state-red)");
@@ -70,7 +122,7 @@ generateBarcode.addEventListener('click', async () => {
     }
 
     clearAnimation = animateGenSkeleton();
-    const genRequest = createForm({'barcode': barcodeInput.value});
+    const genRequest = createForm({ 'barcode': barcodeInput.value });
 
     try {
         const response = await fetch(`${window.location.origin}/app/sticker/generate`, {
@@ -89,15 +141,42 @@ generateBarcode.addEventListener('click', async () => {
         // Convert response to a blob
         const blob = await response.blob();
         const barcodeURL = URL.createObjectURL(blob);
-        barcodeImage.src = barcodeURL;
-        barcodeImage.classList.remove('hidden');
+        barcodePreview.src = barcodeURL;
+        barcodePreview.classList.remove('hidden');
 
         clearAnimation();
         sendToastNotification("Barcode generated.", "thumb_up", "var(--color-state-green)");
 
-        
+        const stickerQuantity = rowInput.value * columnInput.value
+        for (let i = 0; i < stickerQuantity; i++) {
+            const sticker = createSticker();
+            sheetBody.appendChild(sticker);
+        }
+
+
     } catch (error) {
         clearAnimation();
         sendToastNotification("Generation failed, please try again.", "error", "var(--color-state-red)");
     }
+});
+
+// & EVENT LISTENER FOR WINDOW-TOGGLE-TRIGGER CLICK
+windowToggleTriggers.forEach(trigger => {
+    trigger.addEventListener('click', (e) => {
+        const targetSectionClass = e.target.dataset.section;
+        const targetSection = document.querySelector(targetSectionClass);
+        toggleWindow(targetSection);
+    });
+});
+
+// & EVENT LISTENER FOR BODY-SHEET-GRID UPDATE
+addInputChangeListener(columnInput);
+addInputChangeListener(rowInput);
+
+// & INITIAL DISPLAY SETTINGS
+document.addEventListener('DOMContentLoaded', () => {
+    updateBodyGrids(
+        columnInput.value,
+        rowInput.value
+    );
 })
